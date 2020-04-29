191bf3f184d1d013d8f097057f387f6332fd7cb9
Vresod <lukebinkofsky@gmail.com>
2020-04-29 15:53:12 -0400

    added basic discord bot functionality added praw.ini and .vscode/* to .gitignore added ~wip to main.py

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import discord
 import praw # "python reddit api wrapper"
-with open("tokenfile","r") as tokenfile:
-	token = tokenfile.read
+
+with open("tokenfile", "r") as tokenfile:
+	token=tokenfile.read()
 print(token)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+		print("logged in as {0.user}".format(client))
+		status = discord.CustomActivity(name="browsing reddit")
+		await client.change_presence(activity=status, afk=False, status=discord.Status.online)
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+	if message.content.startswith("~wip"):
+		await message.channel.send("This bot is a work in progress.")
+	
+
+client.run(token)
