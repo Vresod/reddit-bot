08fe9911bf2fec14c094643f16175fbd43d6eef7
Vresod <lukebinkofsky@gmail.com>
2020-04-29 17:53:10 -0400

    added basic redditing functionality!

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import discord
 import praw # "python reddit api wrapper"
-
 with open("tokenfile", "r") as tokenfile:
 	token=tokenfile.read()
 print(token)
 
 client = discord.Client()
 reddit = praw.Reddit("bot")
+prefix = "~"
+
 @client.event
 async def on_ready():
 		print("logged in as {0.user}".format(client))
@@ -17,9 +18,14 @@ async def on_ready():
 async def on_message(message):
 	if message.author == client.user:
 		return
-	if message.content.startswith("~wip"):
-		await message.channel.send("This bot is a work in progress.")
-	
-	
+	#if message.content.startswith("~wip"):
+	#	await message.channel.send("This bot is a work in progress.")
+	if message.content.startswith(prefix):
+		args = message.content.replace(prefix,"")
+		argslist = args.split(" ")
+		print(argslist)
+		subreddit = reddit.subreddit(argslist[0])
+		for submission in reddit.subreddit(subreddit).hot(limit=10):
+			await message.channel.send(submission.title)
 
 client.run(token)
