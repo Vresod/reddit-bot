6296e19c6d626c0343f75c4b1959cedd9ff03e95
Vresod <lukebinkofsky@gmail.com>
2020-04-30 15:42:15 -0400

    ~{subreddit} skips pinned posts

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ reddit = praw.Reddit("bot")
 prefix = "~"
 helpmessage = discord.Embed(
 	title="Bot Commands",
-	description="~\{subreddit\} <limit> <listing> (Get posts from a subreddit)\n~bot (Shows this help message)",
+	description="https://github.com/Vresod/reddit-bot\n~\{subreddit\} <limit> <listing> (Get posts from a subreddit)\n~bot (Shows this help message)",
 	color=discord.Color.from_rgb(255, 127, 0)
 )
 
@@ -30,14 +30,17 @@ async def on_message(message):
 	if message.content.startswith(prefix):
 		args = message.content.replace(prefix,"")
 		argslist = args.split(" ")
-		print(argslist)
 		subreddit = reddit.subreddit(argslist[0])
-		print(subreddit)
-		for submission in subreddit.hot(limit=int(argslist[1])):
-			print("{0}\n{1}".format(submission.over_18,message.channel.is_nsfw()))
+		stickiedposts = 0
+		for stickytest in subreddit.hot(limit=2):
+			if(stickytest.stickied):
+				stickiedposts += 1
+		for submission in subreddit.hot(limit=int(argslist[1]) + stickiedposts):
+			if(submission.stickied):
+				continue
 			if(submission.over_18 and not message.channel.is_nsfw()):
 				await message.channel.send("NSFW post. Please try again in an NSFW channel.")
 			else:
-				await message.channel.send("Hot post from {0}: {1} {2}".format(subreddit,submission.title,submission.url))
+				await message.channel.send("Hot post from r/{0}: {1} {2}".format(subreddit,submission.title,submission.url))
 
 client.run(token)
