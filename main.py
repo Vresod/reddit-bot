e5b3c5a5a42f95bef083f00fbb9d14e487c64db0
Vresod <lukebinkofsky@gmail.com>
2020-04-30 16:12:48 -0400

    made ~bot help data driven unsuccessfully

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import discord
 import praw # "python reddit api wrapper"
+import json
+
 with open("tokenfile", "r") as tokenfile:
 	token=tokenfile.read()
-#print(token)
+with open("helpmessage.json", "r") as rawhelpmessage:
+	jsonhelpmessage = json.loads(rawhelpmessage.read())
+	print(jsonhelpmessage)
+	jsonhelpmessage.help.strdescription = "\n".join(jsonhelpmessage.help.description)
 
 client = discord.Client()
 reddit = praw.Reddit("bot")
 prefix = "~"
 helpmessage = discord.Embed(
-	title="Bot Commands",
-	description="https://github.com/Vresod/reddit-bot\n~\{subreddit\} <limit> <listing> (Get posts from a subreddit)\n~bot (Shows this help message)",
-	color=discord.Color.from_rgb(255, 127, 0)
+	title=jsonhelpmessage.help.title,
+	description=jsonhelpmessage.help.strdescription,
+	color=discord.Color.from_rgb(jsonhelpmessage.help.color)
 )
 
 
@@ -24,12 +29,13 @@ async def on_message(message):
 		return
 	#if message.content.startswith("~wip"):
 	#	await message.channel.send("This bot is a work in progress.")
+	args = message.content.replace(prefix,"")
+	argslist = args.split(" ")
 	if message.content.startswith("{0}bot".format(prefix)):
-		await message.channel.send(embed=helpmessage)
+		if(argslist[0] == help):
+			await message.channel.send(embed=helpmessage)
 		return
 	if message.content.startswith(prefix):
-		args = message.content.replace(prefix,"")
-		argslist = args.split(" ")
 		subreddit = reddit.subreddit(argslist[0])
 		stickiedposts = 0
 		for stickytest in subreddit.hot(limit=2):
