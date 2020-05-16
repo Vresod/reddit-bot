e97038ab738a82ed2b2e10f664f0831cb7117f06
Vresod <lukebinkofsky@gmail.com>
2020-05-16 12:25:26 -0400

    added hashbang tried to change server prefix thing to be more efficient; this has not worked yet

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import discord
 import praw # "python reddit api wrapper"
 import json
@@ -18,7 +20,12 @@ if(not extra.file_exists("./runtime/prefixes.json")):
 with open("./runtime/prefixes.json","r") as rawprefixes:
 	prefixes = json.loads(rawprefixes.read())
 
-client = discord.Client()
+istatus = discord.Status.online
+cstatus = discord.Game(name="reddit.com")
+
+print(cstatus.name)
+
+client = discord.Client(status=istatus,activity=cstatus)
 reddit = praw.Reddit("bot")
 
 helpmessage = discord.Embed(title=jsonhelpmessage["help"]["title"],description=jsonhelpmessage["help"]["strdescription"],color=discord.Color.from_rgb(jsonhelpmessage["default_color"][0],jsonhelpmessage["default_color"][1],jsonhelpmessage["default_color"][2]))
@@ -26,12 +33,6 @@ prefixsuccessmessage = discord.Embed(title=jsonhelpmessage["prefix_success"]["ti
 prefixfailuremessage = discord.Embed(title=jsonhelpmessage["prefix_failure"]["title"],description=jsonhelpmessage["prefix_failure"]["strdescription"],color=discord.Color.from_rgb(jsonhelpmessage["default_color"][0],jsonhelpmessage["default_color"][1],jsonhelpmessage["default_color"][2]))
 repomessage = discord.Embed(title=jsonhelpmessage["repo"]["title"],description=jsonhelpmessage["repo"]["strdescription"],color=discord.Color.from_rgb(jsonhelpmessage["default_color"][0],jsonhelpmessage["default_color"][1],jsonhelpmessage["default_color"][2]))
 
-@client.event
-async def on_guild_join(server):
-	prefixes[server.id] = "~" # This is the default prefix
-	prefixesfile = open("./runtime/prefixes.json", "w")
-	prefixesfile.write(json.dumps(prefixes))
-	prefixesfile.close()
 
 @client.event
 async def on_ready():
@@ -41,14 +42,19 @@ async def on_ready():
 async def on_message(message):
 	if message.author == client.user or message.author.bot:
 		return
-	if not message.content.startswith(prefixes[str(message.guild.id)]):
+	serverprefix = ""
+	if hasattr(prefixes, str(message.guild.id)):
+		serverprefix = prefixes[str(message.guild.id)]
+	else:
+		serverprefix = "~"
+	if not message.content.startswith(serverprefix):
 		return
-	args = message.content.replace(prefixes[str(message.guild.id)],"") # args is everything after the prefix
+	args = message.content.replace(serverprefix,"") # args is everything after the prefix
 	argslist = args.split(" ") # gets args as a list. obviously.
 	if(extra.get_sanitized(argslist) == False): # see extra.py
-		await message.channel.send("Your message could not be processed. Make sure you're following the rules defined in `{0}bot help`".format(prefixes[str(message.guild.id)]))
+		await message.channel.send("Your message could not be processed. Make sure you're following the rules defined in `{0}bot help`".format(serverprefix))
 		return
-	if message.content.startswith("{0}bot".format(prefixes[str(message.guild.id)])):
+	if message.content.startswith("{0}bot".format(serverprefix)):
 		if(argslist[1] == "help"):
 			await message.channel.send(embed=helpmessage)
 		if(argslist[1] == "prefix"):
@@ -63,7 +69,7 @@ async def on_message(message):
 		if(argslist[1] == "repo"):
 			await message.channel.send(embed=repomessage)
 		return
-	if message.content.startswith(prefixes[str(message.guild.id)]):
+	if message.content.startswith(serverprefix):
 		subreddit = reddit.subreddit(argslist[0])
 		listing = ""
 		if argslist[2] == "hot":
