f9a291f14547b0f475219b249ab214238742ed7a
Vresod <lukebinkofsky@gmail.com>
2020-08-14 13:16:25 -0400

    added dm support

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ with open("helpmessage.json", "r") as rawhelpmessage:
 	jsonhelpmessage["prefix_success"]["strdescription"] = "\n".join(jsonhelpmessage["prefix_success"]["description"])
 	jsonhelpmessage["prefix_failure"]["strdescription"] = "\n".join(jsonhelpmessage["prefix_failure"]["description"])
 	jsonhelpmessage["repo"]["strdescription"] = "\n".join(jsonhelpmessage["repo"]["description"])
+	jsonhelpmessage["prefix_directmessage"]["strdescription"] = "\n".join(jsonhelpmessage["prefix_directmessage"]["description"])
 
 with open("./runtime/prefixes.json","r") as rawprefixes:
 	prefixes = json.loads(rawprefixes.read())
@@ -34,6 +35,7 @@ helpmessage = discord.Embed(title=jsonhelpmessage["help"]["title"],description=j
 prefixsuccessmessage = discord.Embed(title=jsonhelpmessage["prefix_success"]["title"],description=jsonhelpmessage["prefix_success"]["strdescription"],color=discord.Color.from_rgb(jsonhelpmessage["default_color"][0],jsonhelpmessage["default_color"][1],jsonhelpmessage["default_color"][2]))
 prefixfailuremessage = discord.Embed(title=jsonhelpmessage["prefix_failure"]["title"],description=jsonhelpmessage["prefix_failure"]["strdescription"],color=discord.Color.from_rgb(jsonhelpmessage["default_color"][0],jsonhelpmessage["default_color"][1],jsonhelpmessage["default_color"][2]))
 repomessage = discord.Embed(title=jsonhelpmessage["repo"]["title"],description=jsonhelpmessage["repo"]["strdescription"],color=discord.Color.from_rgb(jsonhelpmessage["default_color"][0],jsonhelpmessage["default_color"][1],jsonhelpmessage["default_color"][2]))
+prefixdirectmessage = discord.Embed(title=jsonhelpmessage["prefix_directmessage"]["title"],description=jsonhelpmessage["prefix_directmessage"]["strdescription"],color=discord.Color.from_rgb(jsonhelpmessage["default_color"][0],jsonhelpmessage["default_color"][1],jsonhelpmessage["default_color"][2]))
 
 
 @client.event
@@ -45,33 +47,37 @@ async def on_message(message):
 	if message.author == client.user or message.author.bot:
 		return
 	serverprefix = ""
-	if (extra.dictionary_contains(prefixes, str(message.guild.id))):
-		serverprefix = prefixes[str(message.guild.id)]
+	if (message.guild != None):
+		if(extra.dictionary_contains(prefixes, str(message.guild.id))):
+			serverprefix = prefixes[str(message.guild.id)]
 	else:
 		serverprefix = "~"
 	if not message.content.startswith(serverprefix):
 		return
 	args = message.content.replace(serverprefix,"") # args is everything after the prefix
 	argslist = args.split(" ") # gets args as a list. obviously.
-	if(not extra.get_sanitized(argslist)): # see extra.py
-		#await message.channel.send("Your message could not be processed. Make sure you're following the rules defined in `{0}bot help`".format(serverprefix))
-		return
+	#if(not extra.get_sanitized(argslist)): # see extra.py
+	#	#await message.channel.send("Your message could not be processed. Make sure you're following the rules defined in `{0}bot help`".format(serverprefix))
+	#	return
 	if message.content.startswith("{0}bot".format(serverprefix)):
 		if(argslist[1] == "help"):
 			await message.channel.send(embed=helpmessage)
-		if(argslist[1] == "prefix"):
-			if(not message.author.permissions_in(message.channel).manage_guild):
-				await message.channel.send(embed=prefixfailuremessage)
-				return
-			prefixes[str(message.guild.id)] = argslist[2]
-			prefixesfile = open("./runtime/prefixes.json", "w")
-			prefixesfile.write(json.dumps(prefixes))
-			prefixesfile.close()
-			await message.channel.send(embed=prefixsuccessmessage)
-		if(argslist[1] == "repo" and not norepo):
+		elif(argslist[1] == "prefix"):
+			if (message.guild != None):
+				if(not message.author.permissions_in(message.channel).manage_guild):
+					await message.channel.send(embed=prefixfailuremessage)
+				prefixes[str(message.guild.id)] = argslist[2]
+				prefixesfile = open("./runtime/prefixes.json", "w")
+				prefixesfile.write(json.dumps(prefixes))
+				prefixesfile.close()
+				await message.channel.send(embed=prefixsuccessmessage)
+			else:
+				await message.channel.send(embed=prefixdirectmessage)
+		elif(argslist[1] == "repo" and not norepo):
 			await message.channel.send(embed=repomessage)
 		return
 	if message.content.startswith(serverprefix):
+		print(serverprefix)
 		subreddit = reddit.subreddit(argslist[0])
 		listing = ""
 		if argslist[2] == "hot":
